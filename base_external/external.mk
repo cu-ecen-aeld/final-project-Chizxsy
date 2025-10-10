0476120fececb833e101c3285d40febf677a2b21
Charles Fischer <cfischer2247@gmail.com>
2025-10-09 20:49:35 -0600

    Assignment 6 Part 2

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,4 @@
+AESD_ASSIGNMENTS_y += aesd-assignments
+LDD_MODULES_y += ldd
+
 include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 030cc12211aec4063a67fe423d761ee3c495ea94
+AESD_ASSIGNMENTS_VERSION = 64336763f318dc15c520bebda47d6506ef694339
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -24,7 +24,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/S99aesdsocket.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
