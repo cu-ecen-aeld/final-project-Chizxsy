4547792a8c5cdb92b4c47c6f8fd14d93cb8a8671
Charles <cfischer2247@gmail.com>
2025-10-13 11:17:11 -0600

    added aesdchar.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 64336763f318dc15c520bebda47d6506ef694339
+AESD_ASSIGNMENTS_VERSION = 716dcca144afcc07f25a3335a831b26d52174396
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,27 @@
+##############################################################
+#
+# AESD-CHAR DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = 716dcca144afcc07f25a3335a831b26d52174396
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Chizxsy.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/S97aesdchar.sh $(TARGET_DIR)/etc/init.d
+
+	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+
+$(eval $(kernel-module))
+
