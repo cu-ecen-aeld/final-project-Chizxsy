bfb49d99b788aaa8fdb9228a061f234ef348ff3d
Charles <cfischer2247@gmail.com>
2025-10-14 15:10:50 -0600

    Assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,22 +4,18 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = a29311ee2d07ce05fb967c192223989a30ea132c
+AESD_ASSIGNMENTS_VERSION = 4f1fb9a2deca4fb6d2beba0f782b027ac9fe2f69
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Chizxsy.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+TARGET_CFLAGS += -DUSE_AESD_CHAR_DEVICE
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) -C $(@D)/finder-app all
     $(MAKE) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    # Install finder scripts and writer utility
-    $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-    $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-    $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-
     # Install files for aesdsocket application
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
--- base_external/package/aesd-char-driver/aesh-char-driver.mk
+++ base_external/package/aesd-char-driver/aesh-char-driver.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = a29311ee2d07ce05fb967c192223989a30ea132c
+AESDCHAR_VERSION = 4f1fb9a2deca4fb6d2beba0f782b027ac9fe2f69
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,12 +16,11 @@ AESDCHAR_GIT_SUBMODULES = YES
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 define AESDCHAR_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)/aesd-char-driver INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
 	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/S97aesdchar.sh $(TARGET_DIR)/etc/init.d
 
 	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/aesdchar_unload $(TARGET_DIR)/usr/bin
-
-    	$(INSTALL) -m 0644 $(@D)/$(AESDCHAR_MODULE_SUBDIRS)/aesdchar.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
 endef
 
 $(eval $(kernel-module))
