a8d6b50696d73c520bab4f7fee2d2fdbb721df92
Charles <cfischer2247@gmail.com>
2025-10-14 21:31:43 -0600

    Assignment 8

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -4,16 +4,16 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESDCHAR_VERSION = 4f1fb9a2deca4fb6d2beba0f782b027ac9fe2f69
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
 AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Chizxsy.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)/aesd-char-driver all
+endef
 
 define AESDCHAR_INSTALL_TARGET_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)/aesd-char-driver INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
@@ -25,3 +25,4 @@ endef
 
 $(eval $(kernel-module))
 
+$(eval $(generic-package))
