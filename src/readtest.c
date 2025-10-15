299cf585b184d65422cd227d0b341f92049f4a88
Charles <cfischer2247@gmail.com>
2025-10-15 15:01:19 -0600

    added read testing code

---
+++ src/readtest.c
@@ -0,0 +1,121 @@
+#include <termios.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFFER_SIZE 1024
+
+int printer_startup(int serial_port, int timeout_sec){
+    char buffer[BUFFER_SIZE];
+    memset(buffer, '\0', sizeof(buffer));
+    int total_read = 0;
+    int read_serial = 0;
+    time_t start_time = time(NULL);
+
+    // On startup Prusa prints to the console:
+    /* start
+    echo: 3.13.3-7094
+    SpoolJoin is Off
+    echo: Last Updated: Feb 27 2024 18:21:24 | Author: (none, default config)
+    echo: Free Memory: 2809 
+    PlannerBufferBytes: 1760
+    echo:Stored settings retrieved
+    adc_init
+    Sending 0xFF
+    echo:SD card ok */
+
+    while ((time(NULL) - start_time) < timeout_sec){
+        read_serial = read(serial_port, &buffer[total_read], sizeof(buffer) - total_read - 1);
+        if (read_serial > 0){
+            total_read += read_serial;
+            printf("--- Printer Startup Output ---\n");
+            printf("%s\n", buffer);
+            printf("------------------------------\n");   
+        }
+    }
+    syslog(LOG_ERR, "Waiting for printer to become ready");
+    return -1;
+
+}
+
+int main(int argc, char *argv[]){
+
+    struct termios tty;
+
+    // open syslog logging
+    openlog("PrintrBoardUSB", LOG_PID, LOG_USER);
+
+    // check for correct number of arguments
+    if (argc < 2){
+        fprintf(stderr, "Incorect number of arguments %s\n", argv[1]);
+        return -1;
+    }
+    
+    char *dev_path = argv[1];
+
+    // open serial to 3d printer
+    int serial_port = open(dev_path, O_RDWR | O_NOCTTY | O_NDELAY);
+
+    if (serial_port < 0){
+        syslog(LOG_ERR, "Failed to open serial port");
+        return -1;
+    }
+
+    // get current attributes
+    if (tcgetattr(serial_port, &tty) != 0){
+        syslog(LOG_ERR, "Failed to store attributes");
+        close(serial_port);
+        return -1;
+    }
+
+    // configure baud rate
+    //tx
+    cfsetospeed(&tty, B115200);
+    //rx
+    cfsetispeed(&tty, B115200);
+    
+    // configure raw mode
+    // void cfmakeraw(serial_port, &tty);
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_oflag &= ~OPOST;
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_cflag &= ~(CSIZE | PARENB);
+    tty.c_cflag |= CS8;
+
+    // sets connection timeout to 10 sec
+    tty.c_cc[VMIN] = 0;
+    tty.c_cc[VTIME] = 10;
+    
+    // set attributes now
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0){
+            syslog(LOG_ERR, "Failed to store updated attributes");
+            close(serial_port);
+            return -1;
+    }
+
+    tcflush(serial_port, TCIOFLUSH); 
+
+    char startup_buffer[BUFFER_SIZE];
+    memset(startup_buffer, '\n', sizeof(startup_buffer));
+
+    if(printer_startup(serial_port, 10) != 0){
+        syslog(LOG_ERR,"Failed to monitor startup");
+        return -1;
+    }
+
+    // ----- for testing -----
+    // report settings with M503
+    /*
+    char *gcode_cmd = "M503\n";
+    syslog(LOG_INFO, "Writing gcode command: %s\n", gcode_cmd);
+    serial_writen(serial_port, gcode_cmd, strlen(gcode_cmd));
+*/  
+    close(serial_port);
+    closelog();
+    return 0;
+
+}
